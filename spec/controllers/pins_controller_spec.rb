c21e90cf0894ac8b8ee78c979b0e7c19f53e0fec
mtaylorelliott <tayls65@yahoo.com>
2017-08-17 17:12:36 -0400

    lord willing spec

--- spec/controllers/pins_controller_spec.rb
+++ spec/controllers/pins_controller_spec.rb
@@ -1,16 +1,12 @@
 require 'spec_helper'
+
 RSpec.describe PinsController do
 
 	describe "GET index" do
 		it 'renders the index template' do
 			get :index
 			expect(response).to render_template("index")
-        end
-	end
+		end
 
-	it 'populates @pins with all the pins' do
-		get :index
-		expect(assigns[:pins]).to eq(Pin.all)
 	end
-
 end
