e15cab82032bfbaa5c906f9087a64639e8d9308d
mtaylorelliott <tayls65@yahoo.com>
2017-09-29 12:44:08 -0400

    form helpers

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -13,6 +13,23 @@ class PinsController < ApplicationController
         render :show
     end
     
+    def new
+        @pin = Pin.new
+    end
     
+    def create
+        @pin = Pin.create(pin_params)
+
+        if @pin.valid?
+            @pin.save
+            redirect_to pin_path(@pin)
+        else
+            @errors = @pin.errors
+            render :new
+    end
+
+    def pin_params
+        params.require(:pin).permit(:title, :url, :slug, :text :category_id)
+    end
 end
   
