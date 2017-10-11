a71d145b764b68ef654eaeb1c9959c725633ec8a
mtaylorelliott <tayls65@yahoo.com>
2017-10-11 00:47:56 -0500

    specs added with errors in edit method

--- spec/controllers/pins_controller_spec.rb
+++ spec/controllers/pins_controller_spec.rb
@@ -80,6 +80,26 @@ RSpec.describe PinsController do
       post :create, pin: @pin_hash
       expect(assigns[:errors].present?).to be(true)
     end    
-    
+  describe "GET edit" do
+    before(:each) do
+      @pin = Pin.find(3)
+    end
+
+      it 'responds successfully' do
+      get :edit, id: @pin.id
+      expect(response.success?).to be(true)
+      end
+
+      it 'renders the edit template' do
+      get :edit, id: @pin.id
+      expect(response.success?).to be(true)
+      end
+
+      it 'assigns an instance variable called @pin to the pin with the appropriate id' do
+      get :edit, id: @pin.id
+      expect(response.success?).to be(true)
+    end
+  end
+
   end
 end
