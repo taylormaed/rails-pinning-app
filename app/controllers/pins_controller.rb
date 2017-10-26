dbd295ebed3927c6634f6afdd8d37d91fe6ec2f8
mtaylorelliott <tayls65@yahoo.com>
2017-10-25 20:41:55 -0400

    params require changes

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -47,7 +47,7 @@ class PinsController < ApplicationController
 
     private
     def pin_params
-        params.require(:pin).permit( :title, :url, :slug, :text, :category_id, :image)
+        params.require(:pin).permit(:title, :url, :slug, :text, :category_id, :image, images_attributes: [:image]) 
     end
 
 end
