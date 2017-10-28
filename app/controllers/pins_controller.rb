6c92aaacc5e3f85cb606cdd99ae0b465aeab43bb
mtaylorelliott <tayls65@yahoo.com>
2017-10-27 23:24:35 -0400

    form works prior to heroku image setting changes

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,55 +1,51 @@
 class PinsController < ApplicationController
-    
-    def index
-        @pins = Pin.all
-    end
-    
-    def show
-        @pin = Pin.find(params[:id])
-    end
-    
-    def show_by_name
-        @pin = Pin.find_by_slug(params[:slug])
-        render :show
-    end
-    
-    def new
-        @pin = Pin.new
-    end
-    
-    def create
-        @pin = Pin.create(pin_params)
+  def index
+    @pins = Pin.all
+  end
 
-        if @pin.valid?
-            @pin.save
-            redirect_to pin_path(@pin)
-        else
-            @errors = @pin.errors
-            render :new
-        end
-    end
+  def show
+    @pin = Pin.find(params[:id])
+  end
 
-    
-    def edit
-        @pin = Pin.find(params[:id])
-        render :edit
-    end
+  def show_by_name
+    @pin = Pin.find_by_slug(params[:slug])
+    render :show
+  end
 
-    def update
-        @pin = Pin.find(params[:id])
-            if @pin.update_attributes(pin_params)
-                redirect_to pin_path(@pin), notice: "Pin has been updated successfully."
-            else
-                @errors = @pin.errors
-                render action: "edit"
-            end
-    end
+  def new
+    @pin = Pin.new
+  end
 
-    private
-    def pin_params
-        params.require(:pin).permit(:title, :url, :slug, :text, :category_id, :image, images_attributes: [:image]) 
-    end
+  def create
+    @pin = Pin.create(pin_params)
 
+    if @pin.valid?
+      @pin.save
+      redirect_to pin_path(@pin)
+    else
+      @errors = @pin.errors
+      render :new
+    end
+  end
+
+  def edit
+    @pin = Pin.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @pin = Pin.find(params[:id])
+    if @pin.update_attributes(pin_params)
+      redirect_to pin_path(@pin), notice: 'Pin has been updated successfully.'
+    else
+      @errors = @pin.errors
+      render action: 'edit'
+    end
+  end
+
+private
+
+  def pin_params
+    params.require(:pin).permit(:title, :url, :slug, :text, :category_id, :image, images_attributes: [:image])
+  end
 end
-
-
